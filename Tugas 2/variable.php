9279147d31b7c16bead7565e7ee4b8b5a469b2fc
fikri <erhafikri@gmail.com>
2022-03-09 09:38:52 +0700

    Menambah File Variable.php Di Folder Tugas 2

---
+++ Tugas 2/variable.php
@@ -0,0 +1,16 @@
+<?php
+$angka1 = 10;
+$angka2 = 20;
+$angka3 = 30;
+
+$penjumlahan = $angka1 + $angka2;
+$pengurangan = $angka3 - $angka2;
+$perkalian = $angka3 * $angka1;
+
+echo "Hasil Penjumlahan Dari ". $angka1 ." + ". $angka2 ." = " . $penjumlahan;
+echo '<br>';
+echo "Hasil Penjumlahan Dari ". $angka3 ." - ". $angka2 ." = " . $pengurangan;
+echo '<br>';
+echo "Hasil Penjumlahan Dari ". $angka3 ." x ". $angka1 ." = " . $perkalian;
+
+?>
