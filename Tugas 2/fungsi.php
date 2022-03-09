11a0411c5b5be2f2ae2cc20f4cb77a49273713a6
fikri <erhafikri@gmail.com>
2022-03-09 09:58:28 +0700

    Menambah File Fungsi.php di Folder Tugas 2

---
+++ Tugas 2/fungsi.php
@@ -0,0 +1,13 @@
+<?php
+
+function makanan($nama, $makanan){
+    echo "Halo Nama Saya ". $nama . " dan makanan favorit saya adalah ". $makanan;
+}
+
+makanan("Fikri Ainun Najib","Nasi Goreng");
+echo "<hr>";
+makanan("Caca Handika","Tempe Goreng");
+echo "<hr>";
+makanan("Cici Lalang","Bakso");
+echo "<hr>";
+?>
